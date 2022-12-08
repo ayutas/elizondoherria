7e9ddcb3fbd26956f6643b35f29ac0229aaacd2c
Antxon Mugica <antxonm@anasinf.com>
2022-12-08 19:08:28 +0100

    [FIX] Quito boton de crear xml ya que se hace al crear los recibos

--- app/Views/recibos/opciones.php
+++ app/Views/recibos/opciones.php
@@ -8,9 +8,8 @@
                 <!-- titulo -->
                 <h1>Recibos</h1>
                 <div class="row col-12 mt-4">
-                    <div class="col-4"><a href="<?= base_url()?>/newrecibos" class="btn btn-primary tablasbtn">RECIBOS</a></div>
-                    <div class="col-4"><a href="<?= base_url()?>/crearxml" class="btn btn-primary tablasbtn">XML</a></div>
-                    <div class="col-4"><a href="<?= base_url()?>/consultas" class="btn btn-primary tablasbtn">CONSULTAS</a></div>
+                    <div class="col-6"><a href="<?= base_url()?>/newrecibos" class="btn btn-primary tablasbtn">CREAR RECIBOS</a></div>
+                    <div class="col-6"><a href="<?= base_url()?>/consultas" class="btn btn-primary tablasbtn">CONSULTAS</a></div>
                 </div>
             </div>
         </div>
